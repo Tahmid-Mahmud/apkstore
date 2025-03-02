5d402d08f49686bebe80e2a371a7aeb2dc05ad4b
Tahmid Mahmud <73707938+Tahmid-Mahmud@users.noreply.github.com>
2025-03-02 17:23:05 +0600

    B

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const data={
 "Kivy Calculator":{
      "icon": "kivy-calc/calc.jpg",
      "size": "15MB",
-     "apk": "apps/kivy-calc.apk",
+     "apk": "https://download1479.mediafire.com/xiaf3q9uumugSFXTifFkZR4XXSysgjtIoqsirnnEKHU7ZfASYubaXWt4qbTnf07ITZmHzihtGUvsv5SRojjUaxUsr5EYFgtPspGJlGC5r7zQucRmuOJpY1diWQ130pWki2zuztyYktTg-jd3Q4xWjOeYhn0xrxmkkNp-aaQsCTwMBSCB/0sghm3f36wkt3la/kivy_calc.apk",
      "screenshot":{
          "ss1": "kivy-calc/ss1.png",
          "ss2": "kivy-calc/ss2.png",
@@ -17,7 +17,7 @@ const data={
 "PyTerminal":{
      "icon": "pyterminal/term.png",
      "size": "17MB",
-     "apk": "apps/term.apk",
+     "apk": "https://download1323.mediafire.com/24d54oh4aucgatyW9JWsAfQDurkyAf94nsJlNN2WMKbCx2vfG8GebAZM0O-dOaVjpzD0ufHw01fHzjzeRbiM9T5seCUoES1nHUfXxGW3eZQIM3kTAksMSeYB4BTpztKYbXj2oPJoVWLpd-RWtc9psL-M9MlN4YTyJurOPEaSvvtywQqU/nsjwjdvjbsm4x4a/term.apk",
      "screenshot":{
          "ss1": "pyterminal/ss1.png",
          "ss2": "pyterminal/ss2.png",
@@ -30,7 +30,7 @@ const data={
 "Tic Tac Toe":{
      "icon": "tic-tac-toe/ttt.png",
      "size": "21MB",
-     "apk": "apps/ttt.apk",
+     "apk": "https://download946.mediafire.com/88s3uy4ll1fgeQX8dRiPx-hZWWCcit-v1T5HillL3DzEi9aJExkTaB0K_XOq_owvY31OBzozfHIRM5s2tfK_5Yp9VJoI9kZBfay7u5Tpaa1XYXJUf9j1Rz0ZhG_tIFWe0ZalrE84kvOYzRnQLU3mQewJL-cqHFPk6AbLrspT9zghhYlg/4z49xym3mxt8h6x/ttt.apk",
      "screenshot":{
          "ss1": "tic-tac-toe/ss1.jpg",
          "ss2": "tic-tac-toe/ss2.jpg",
@@ -59,7 +59,7 @@ const data={
 "Planet Shooter":{
      "icon": "https://tahmid-mahmud.github.io/planet-shooter/img/icon.jpg",
      "size": "15MB",
-     "apk": "apps/planet.apk",
+     "apk": "https://download1076.mediafire.com/irfkhhpbfwlgH35laJH3NYbRGjMn2mEiaz7-idctIlZx67Q7X-VX02h2dZpaSuYPnuWd-OPJ5asV2Tbm2mZjMKZq1rzdxCmhQf_wHziiq8FI5TJK0ymJXGB7_apcmMsSJgVXPhKL-Ix2Nqkr1Wl816sYxv0rGb6ZeB9HmFC5TF33DqjN/6gygb7osfspefmu/planet.apk",
      "screenshot":{
          "ss1": "Planet Shooter/ss1.webp",
          "ss2": "Planet Shooter/ss2.webp",
@@ -73,7 +73,7 @@ const data={
 "PyCulator":{
      "icon": "pycalc/icon.png",
      "size": "13MB",
-     "apk": "apps/pycalc.apk",
+     "apk": "https://download1326.mediafire.com/sn3k3yf0x1vgyKwUKbjMiXCrwPKuijbsxhl1ng_RDo5BywN9A45dKYuMnLwIYYBYyQR6AyMiRqKNeMTq3QoK-h1hgZhpufoN0QSGzlN3-3rqYraNRU7U903CfsLgeSupRaT1cdra1cybxzV8UBGQiMDDX_vtbZAt7HbWodel8vkd8E6s/tqf3fxoh5jussnq/pycalc.apk",
      "screenshot":{
          "ss1": "pycalc/ss1.jpg",
          "ss2": "pycalc/ss2.jpg",
